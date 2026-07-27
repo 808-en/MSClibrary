635c460ff0411ff86d1c2d79ed09bc749dccee35
sticky_tape <204648342+808-en@users.noreply.github.com>
2026-07-27 14:10:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -84,6 +84,22 @@ document.addEventListener('DOMContentLoaded', function() {
 
 });
 
+function toggleDropdown() {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
+
+    window.onclick = function(event) {
+        if (!event.target.matches('.dropdown button')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            for (var i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+
 let shouldNavigate = false;
 
 function alertRec() {
